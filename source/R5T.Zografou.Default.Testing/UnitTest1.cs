770568bb522d524f317b268b469c17c758cc73ef
MinexAutomation <minex.automation@gmail.com>
2020-01-22 00:12:13 -0800

    Initial commit of source-stub.

---
+++ source/R5T.Zografou.Default.Testing/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace R5T.Zografou.Default.Testing
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
