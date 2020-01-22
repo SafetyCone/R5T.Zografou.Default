1318aa2b8dadb1303de203956c349c0c6d64d1d7
MinexAutomation <minex.automation@gmail.com>
2020-01-22 00:15:21 -0800

    Added default TestingDataDirectoryNameConvention of the ITestingDataDirectoryNameConvention service.

---
+++ source/R5T.Zografou.Default/Code/Services/Implementations/TestingDataDirectoryNameConvention.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+namespace R5T.Zografou.Default
+{
+    public class TestingDataDirectoryNameConvention : ITestingDataDirectoryNameConvention
+    {
+        public const string TestingDataDirectoryName = "Testing Data";
+
+
+        public string GetTestingDataDirectoryName()
+        {
+            return TestingDataDirectoryNameConvention.TestingDataDirectoryName;
+        }
+    }
+}
