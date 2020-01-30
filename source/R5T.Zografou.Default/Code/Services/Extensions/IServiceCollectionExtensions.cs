a06f488191decb62958f386808336b3fa9ee9931
MinexAutomation <minex.automation@gmail.com>
2020-01-30 14:35:40 -0800

    Added IServiceCollection extensions.

---
+++ source/R5T.Zografou.Default/Code/Services/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using R5T.Dacia;
+
+
+namespace R5T.Zografou.Default
+{
+    public static class IServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Add the <see cref="TestingDataDirectoryNameConvention"/> implementation of <see cref="ITestingDataDirectoryNameConvention"/> as a <see cref="ServiceLifetime.Singleton"/>.
+        /// </summary>
+        public static IServiceCollection AddDefaultTestingDataDirectoryNameConvention(this IServiceCollection services)
+        {
+            services.AddSingleton<ITestingDataDirectoryNameConvention, TestingDataDirectoryNameConvention>();
+
+            return services;
+        }
+
+        /// <summary>
+        /// Add the <see cref="TestingDataDirectoryNameConvention"/> implementation of <see cref="ITestingDataDirectoryNameConvention"/> as a <see cref="ServiceLifetime.Singleton"/>.
+        /// </summary>
+        public static ServiceAction<ITestingDataDirectoryNameConvention> AddDefaultTestingDataDirectoryNameConventionAction(this IServiceCollection services)
+        {
+            var serviceAction = new ServiceAction<ITestingDataDirectoryNameConvention>(() => services.AddDefaultTestingDataDirectoryNameConvention());
+            return serviceAction;
+        }
+    }
+}
